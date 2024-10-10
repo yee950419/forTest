4ed10f7ddf5cfe3d7fc9b1adad3d1bafc1f93d3a
sanghak <yee950419@naver.com>
2024-10-10 16:43:35 +0900

    something changed

--- MSA toy project/first-service/src/main/java/com/ecommerce/first_service/welcome/FirstServiceController.java
+++ MSA toy project/first-service/src/main/java/com/ecommerce/first_service/welcome/FirstServiceController.java
@@ -1,4 +1,18 @@
 package com.ecommerce.first_service.welcome;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/first-service")
+@Slf4j
+@RestController
 public class FirstServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+
+        return "welcome to the first service!";
+    }
 }
--- MSA toy project/second-service/src/main/java/com/ecommerce/second_service/welcome/SecondServiceController.java
+++ MSA toy project/second-service/src/main/java/com/ecommerce/second_service/welcome/SecondServiceController.java
@@ -1,4 +1,18 @@
 package com.ecommerce.second_service.welcome;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/second-service")
+@Slf4j
+@RestController
 public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+
+        return "welcome to the second service!";
+    }
 }
